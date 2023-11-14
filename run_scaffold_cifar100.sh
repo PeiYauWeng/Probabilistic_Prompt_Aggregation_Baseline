49b3632b0ce08bfc8a6b4b8fc96aa8b9717525db
Pei-Yau Weng pei-yau.weng@wsu.edu <pei-yau.weng@wsu.edu>
2023-11-14 11:31:20 +0000

    modify lr in scaffold

--- run_scaffold_cifar100.sh
+++ run_scaffold_cifar100.sh
@@ -1,6 +1,6 @@
-python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --alpha 0.5
-python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --alpha 0.4
-python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --alpha 0.3
-python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --alpha 0.2
-python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --alpha 0.1
-python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --data_distribution manual_extreme_heterogeneity --n_dominated_class 10
+python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --lr 1e-4 --alpha 0.5
+python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --lr 1e-4 --alpha 0.4
+python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --lr 1e-4 --alpha 0.3
+python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --lr 1e-4 --alpha 0.2
+python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --lr 1e-4 --alpha 0.1
+python3 -u main.py --alg scaffold --dataset cifar100 --device cuda:$1 --comms 200 --lr 1e-4 --data_distribution manual_extreme_heterogeneity --n_dominated_class 10
